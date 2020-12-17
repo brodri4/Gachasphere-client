107879ad9dc2ce256ce47d8f45faee3d3127b118
abbeyperini <abbeyperini@gmail.com>
2020-12-17 11:56:58 -0500

    whoops - login/reg

---
+++ src/store/actions/userActions.js
@@ -0,0 +1,34 @@
+import { userConstants } from './userActionTypes';
+import { userService } from './userService';
+import history from '../../utils/history';
+import { setAuthenticationHeader } from '../../utils/authenticate';
+
+export const userActions = {
+    login
+}
+
+function login(user) {
+    return dispatch => {
+        userService.login(user)
+        .then(
+            result => {
+                if (result.data.login === true) {
+                    const token = result.data.token;
+                    localStorage.setItem('jsonwebtoken', token);
+                    setAuthenticationHeader(token);
+                    dispatch(success());
+                    history.push('/dashboard');
+                } else if (result.data.login === false) {
+                    let error = "Username or password is incorrect."
+                    dispatch(failure(error))
+                }
+            },
+            error => {
+                dispatch(failure(error))
+            }
+        )
+    }
+
+    function success() { return { type: userConstants.LOGIN_SUCCESS } }
+    function failure(error) { return { type: userConstants.LOGIN_FAILURE, payload: error } }
+}
