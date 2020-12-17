4a53644bf5aae04f4c500491be512a35155a9132
abbeyperini <abbeyperini@gmail.com>
2020-12-17 12:36:03 -0500

    login/reg first pass

--- src/index.js
+++ src/index.js
@@ -3,10 +3,48 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { HashRouter, Switch, Route, Redirect } from 'react-router-dom';
+import { compose, createStore, applyMiddleware, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
+import userReducer from './store/reducers/userReducer';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import { PersistGate } from 'redux-persist/integration/react';
+import hardSet from 'redux-persist/lib/stateReconciler/hardSet';
+import thunk from 'redux-thunk';
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  stateReconciler: hardSet
+}
+
+const rootReducer = combineReducers({
+  userR: userReducer
+})
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(persistedReducer, composeEnhancers(applyMiddleware(thunk)));
+const persistor = persistStore(store);
+
+// index route leads to login page, assuming using app component for it
+// <Route exact path="/register" component={Register} />
+// <Route exact path="/dashboard" component={requireAuth(Dashboard)} />
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <HashRouter>
+          <Switch>
+            <Route exact path="/index" component={App} />
+            <Redirect exact from='/' to='/index' />
+          </Switch>
+        </HashRouter>
+      </PersistGate>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ src/store/reducers/userReducer.js
@@ -0,0 +1,37 @@
+import { userConstants } from '../actions/userActionTypes';
+
+const initialState = { isAuthenticated: false };
+
+const reducer = (state = initialState, action) => {
+    switch(action.type) {
+        case userConstants.LOGIN_SUCCESS:
+            return {
+                ...state,
+                isAuthenticated: true
+            }
+        case userConstants.LOGIN_FAILURE:
+            return {
+                ...state,
+                isAuthenticated: false,
+                error: action.payload
+            }
+        case userConstants.REGISTER_SUCCESS:
+            return {
+                ...state,
+                isAuthenticated: true
+            }
+        case userConstants.REGISTER_FAILURE:
+            return {
+                ...state,
+                isAuthenticated: false,
+                error: action.payload
+            }
+        // all reducers will need to listen for this
+        case userConstants.LOGOUT:
+            return {
+                initialState
+            }
+        default:
+            return state
+    }
+}
