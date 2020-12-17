746fd23b8d2b0b79c3ac6f9b08c7be20bc8c2ae1
abbeyperini <abbeyperini@gmail.com>
2020-12-17 14:45:08 -0500

    useAuth fix

--- src/components/requireAuth.js
+++ src/components/requireAuth.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
-export default function(ComposedComponent) {
+export default function foo(ComposedComponent) {
 
     class Authenticate extends Component {
         constructor(props) {
