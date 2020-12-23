c34c45918bd4b8b071c6d57bba9ab11806fa7249
Borinquen  Rodriguez <boris@Borinquens-MacBook-Pro.local>
2020-12-23 16:23:15 -0500

    added twitter api

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import Article from './components/Article';
+import { TwitterTimelineEmbed } from 'react-twitter-embed'
 import '../index.css'
 
 function Game() {
   const [articles, setArticles] = useState([]);
-  //Pass in the subreddit name and remove input box
+  //Pass in the subreddit name, twitter name
   const [subreddit, setSubreddit] = useState('OnePieceTC');
   useEffect(() => {
     fetch("https://www.reddit.com/r/" + subreddit + "/hot" + ".json").then(
@@ -22,11 +23,19 @@ function Game() {
   }, [subreddit]);
 
   return (
-    <div className="App">
+    <div class="gameComponent">
       <div class="articles">
         <h3>Reddit</h3>
         {(articles != null) ? articles.map((article, index) => <Article key={index} article={article.data} />) : ''}
       </div>
+      <div class="articles">
+      <TwitterTimelineEmbed
+  sourceType="profile"
+  //Pass in twitter name here
+  screenName="ONE_PIECE_TC"
+  options={{height: 800}, {width:350}}
+/>
+</div>
     </div>
   );
 }
