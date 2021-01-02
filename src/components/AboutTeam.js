f39b2ed8f47de06780da6b5af7189aca487bfb4f
Jonathan Nuno <jnuno.423@gmail.com>
2021-01-02 16:07:39 -0500

    added aboutus info for hung, jonathan and boris, updated aboutpage for footer to be at bottom

--- src/components/AboutTeam.js
+++ src/components/AboutTeam.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import abbey from "../images/abbey.jpg";
+import jonathan from '../images/jnuno-profile.png';
+import hung from '../images/hung.JPG';
+import boris from '../images/boris.jpg';
 import { ReactComponent as ChromeLogo } from '../images/chrome.svg';
 import { ReactComponent as GithubLogo } from '../images/github.svg';
 import { ReactComponent as LinkedinLogo } from '../images/linkedin.svg';
@@ -12,16 +15,16 @@ function AboutTeam() {
                 <div className="team_card_inner">
                     <div className="team_card_inner_front">
                         <div className="image-cropper">
-                            {/* <img className="headshot" src={boris} alt="headshot of Borinquen 'Boris' Rodriguez"></img> */}
+                            <img className="headshot" src={boris} alt="headshot of Borinquen 'Boris' Rodriguez"></img>
                         </div>
                         <h4 className="team_card_inner_front_name">Borinquen "Boris" Rodriguez</h4>
                     </div>
                     <div className="team_card_inner_back">
                         <p>Boris is...</p>
-                        <a href="#" target="_blank" rel="noreferrer"><LinkedinLogo className="linkIcon" alt="LinkedIn logo link to LinkedIn profile"/></a> 
-                        <a href="#" target="_blank" rel="noreferrer"><GithubLogo className="linkIcon" alt="Github logo link to github profile"/></a>
-                        <a href="#" target="_blank" rel="noreferrer"><ChromeLogo className="linkIcon" alt="Chrome logo link to portfolio"/></a>
-                        <a href="#" target="_blank" rel="noreferrer"><TwitterLogo className="linkIcon" alt="Twitter logo link to Twitter profile"/></a>
+                        <a href="https://www.linkedin.com/in/brodri4/" target="_blank" rel="noreferrer"><LinkedinLogo className="linkIcon" alt="LinkedIn logo link to LinkedIn profile"/></a> 
+                        <a href="https://github.com/brodri4" target="_blank" rel="noreferrer"><GithubLogo className="linkIcon" alt="Github logo link to github profile"/></a>
+                        <a href="http://borinquen-rodriguez-portfolio.surge.sh/" target="_blank" rel="noreferrer"><ChromeLogo className="linkIcon" alt="Chrome logo link to portfolio"/></a>
+                        <a href="https://twitter.com/Brodri4" target="_blank" rel="noreferrer"><TwitterLogo className="linkIcon" alt="Twitter logo link to Twitter profile"/></a>
                     </div>
                 </div>
             </div>
@@ -46,16 +49,15 @@ function AboutTeam() {
                 <div className="team_card_inner">
                     <div className="team_card_inner_front">
                         <div className="image-cropper">
-                            {/* <img className="headshot" src={hung} alt="headshot of Hung Tran"></img> */}
+                            <img className="headshot" src={hung} alt="headshot of Hung Tran"></img>
                         </div>
                         <h4 className="team_card_inner_front_name">Hung Tran</h4>
                     </div>
                     <div className="team_card_inner_back">
                         <p>Hung is...</p>
-                        <a href="#" target="_blank" rel="noreferrer"><LinkedinLogo className="linkIcon" alt="LinkedIn logo link to LinkedIn profile"/></a> 
-                        <a href="#" target="_blank" rel="noreferrer"><GithubLogo className="linkIcon" alt="Github logo link to github profile"/></a>
-                        <a href="#" target="_blank" rel="noreferrer"><ChromeLogo className="linkIcon" alt="Chrome logo link to portfolio"/></a>
-                        <a href="#" target="_blank" rel="noreferrer"><TwitterLogo className="linkIcon" alt="Twitter logo link to Twitter profile"/></a>
+                        <a href="https://www.linkedin.com/in/hungtran0713/" target="_blank" rel="noreferrer"><LinkedinLogo className="linkIcon" alt="LinkedIn logo link to LinkedIn profile"/></a> 
+                        <a href="https://github.com/hptran7" target="_blank" rel="noreferrer"><GithubLogo className="linkIcon" alt="Github logo link to github profile"/></a>
+                        <a href="https://hptran7.github.io/Porfolio/" target="_blank" rel="noreferrer"><ChromeLogo className="linkIcon" alt="Chrome logo link to portfolio"/></a>
                     </div>
                 </div>
             </div>
@@ -63,16 +65,15 @@ function AboutTeam() {
                 <div className="team_card_inner">
                     <div className="team_card_inner_front">
                         <div className="image-cropper">
-                            {/* <img className="headshot" src={jonathan} alt="headshot of Jonathan Nuno"></img> */}
+                            <img className="headshot" src={jonathan} alt="headshot of Jonathan Nuno"></img>
                         </div>
                         <h4 className="team_card_inner_front_name">Jonathan Nuno</h4>
                         </div>
                     <div className="team_card_inner_back">
-                        <p>Jonathan is...</p>
-                        <a href="#" target="_blank" rel="noreferrer"><LinkedinLogo className="linkIcon" alt="LinkedIn logo link to LinkedIn profile"/></a> 
-                        <a href="#" target="_blank" rel="noreferrer"><GithubLogo className="linkIcon" alt="Github logo link to github profile"/></a>
-                        <a href="#" target="_blank" rel="noreferrer"><ChromeLogo className="linkIcon" alt="Chrome logo link to portfolio"/></a>
-                        <a href="#" target="_blank" rel="noreferrer"><TwitterLogo className="linkIcon" alt="Twitter logo link to Twitter profile"/></a>
+                        <p>Jonathan is a Full-Stack Web Developer whose always looking for a puzzle to solve!</p>
+                        <a href="https://www.linkedin.com/in/jonathannuno/" target="_blank" rel="noreferrer"><LinkedinLogo className="linkIcon" alt="LinkedIn logo link to LinkedIn profile"/></a> 
+                        <a href="https://github.com/Jonathan-Nuno" target="_blank" rel="noreferrer"><GithubLogo className="linkIcon" alt="Github logo link to github profile"/></a>
+                        <a href="https://jonathan-nuno.dev/" target="_blank" rel="noreferrer"><ChromeLogo className="linkIcon" alt="Chrome logo link to portfolio"/></a>
                     </div>
                 </div>
             </div>
