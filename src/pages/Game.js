1201c6a2328283f72df4241ef9ed56d0df28f837
Jonathan Nuno <jnuno.423@gmail.com>
2021-01-05 09:07:16 -0500

    added css for game details page, fixed footer, made columns consistent with rest of website

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -11,7 +11,7 @@ function Game(props) {
   const gameId = parseInt(useParams().gameId)
   const [articles, setArticles] = useState([]);
   const [game, setGame] = useState({
-    averageRating: null, 
+    averageRating: null,
     averageF2P: null
   });
 
@@ -21,45 +21,46 @@ function Game(props) {
   }, [game]);
 
   const findGame = (gameId) => {
-    const currentGame = props.games.find(game => 
+    const currentGame = props.games.find(game =>
       game.id === gameId
-      )
-      setGame(currentGame)
+    )
+    setGame(currentGame)
     return;
   }
 
   const fetchReddit = () => {
-    if(game.reddit){
-    fetch("https://www.reddit.com/r/" + game.reddit + "/hot" + ".json").then(
-      res => {
-        if (res.status !== 200) {
-          console.warn("Warning: Something is wrong with the api.");
-          return;
+    if (game.reddit) {
+      fetch("https://www.reddit.com/r/" + game.reddit + "/hot" + ".json").then(
+        res => {
+          if (res.status !== 200) {
+            console.warn("Warning: Something is wrong with the api.");
+            return;
+          }
+          res.json().then(data => {
+            if (data != null)
+              setArticles(data.data.children);
+          });
         }
-        res.json().then(data => {
-          if (data != null)
-            setArticles(data.data.children);
-        });
-      }
-    )
-    }else{
+      )
+    } else {
       return null
     }
   }
 
   if (!props.games || !props.games[0]) {
     return (
-        <div>
-            <h1>Loading!</h1>
-        </div>
+      <div>
+        <h1>Loading!</h1>
+      </div>
     )
   } else {
-      let altText = `${props.games.title} logo`
-      return (
-        <div className="gameComponent">
-          <div className='articles'>
+    let altText = `${props.games.title} logo`
+    return (
+      <div className="gameComponent">
+        <div className='articles'>
+          <h2 className="articles_secondary-heading">{game.title}</h2>
+          <div className='articles_game-details-list'>
             <img src={game.logo} alt={altText} className="game-item_game_logo" />
-            <h2 className="secondary-heading">{game.title}</h2>
             <p className="developer">Developer: {game.developer}</p>
             <p className="released">Released: {game.releaseDate}</p>
             {game.averageRating === null ? null : <p className="average-rating">Average Gameplay Rating:</p>}
@@ -68,23 +69,28 @@ function Game(props) {
             {game.averageF2P === null ? null : <p className="average">{game.averageF2P.toFixed(1)}/10</p>}
             {game.iOS == null ? null : <a href={game.iOS} target="_blank" ><img src={iOS} className="dl-button2"></img></a>}
             {game.Android == null ? null : <a href={game.Android} target="_blank" ><img src={Android} className="dl-button"></img></a>}
-
-
           </div>
-          <div className="articles">
-            <h3>Reddit</h3>
+
+        </div>
+        <div className="articles">
+          <h2 className='articles_secondary-heading'>Reddit</h2>
+          <div className='articles_list'>
             {(articles != null) ? articles.map((article, index) => <Article key={index} article={article.data} />) : ''}
           </div>
-          <div className="articles">
-          <TwitterTimelineEmbed
-      sourceType="profile"
-      screenName={game.twitter}
-      options={{height: 800}, {width:350}}
-    />
-    </div>
         </div>
-      );
-    }
+        <div className="articles">
+          <h2 className='articles_secondary-heading'>Twitter</h2>
+          <div className='articles_list'>
+            <TwitterTimelineEmbed
+              sourceType="profile"
+              screenName={game.twitter}
+              options={{ height: 800 }, { width: 350 }}
+            />
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 const mapStateToProps = (state) => {
